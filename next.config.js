4a84787dd2ea39ad22e6f0fb8c796c30ac666f48
arthur791004 <arthur791004@gmail.com>
2019-08-19 15:10:48 +0800

    introduce @svg/webpack

--- next.config.js
+++ next.config.js
@@ -1,9 +1,47 @@
 require('dotenv').config();
 
-const config = {
-  env: {
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+const babelLoader = {
+  loader: 'babel-loader',
+};
+
+const svgrLoader = {
+  loader: '@svgr/webpack',
+  options: {
+    babel: false,
+    svgoConfig: {
+      plugins: {
+        removeViewBox: false,
+      },
+    },
   },
+};
+
+const urlLoader = {
+  loader: 'url-loader',
+  options: {
+    limit: 1024,
+  },
+};
+
+const env = {
+  GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+};
+
+const webpack = config => {
+  config.module.rules.unshift({
+    test: /\.svg$/,
+    issuer: {
+      test: /\.js$/,
+    },
+    use: [babelLoader, svgrLoader, urlLoader],
+  });
+
+  return config;
+};
+
+const config = {
+  env,
+  webpack,
 };
 
 module.exports = config;
